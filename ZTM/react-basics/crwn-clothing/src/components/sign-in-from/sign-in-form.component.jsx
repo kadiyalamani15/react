0157f711731a98019b90b019e6a5c0cbecc51444
Kadiyala Mani <kadiyala.mani15@gmail.com~>
2023-04-26 18:57:23 +0530

    centralised the authorization component

--- ZTM/react-basics/crwn-clothing/src/components/sign-in-from/sign-in-form.component.jsx
+++ ZTM/react-basics/crwn-clothing/src/components/sign-in-from/sign-in-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState} from "react";
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
@@ -18,26 +18,24 @@ const defaultFormFields = {
 
 const SignInForm = () => {
 	const [formFields, setFormFields] = useState(defaultFormFields);
-	const { email, password } = formFields;
+    const { email, password } = formFields;
 
 	const resetFormFields = () => {
 		setFormFields(defaultFormFields);
 	};
 
 	const signInWithGoogle = async () => {
-		const { user } = await signInWithGooglePopup();
-		await createUserDocumentFromAuth(user);
+		await signInWithGooglePopup();
 	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		try {
-			const response = await signInAuthUserWithEmailAndPassword(
+			const {user} = await signInAuthUserWithEmailAndPassword(
 				email,
 				password
-			);
-			console.log(response);
+            );
 			resetFormFields();
 		} catch (err) {
 			switch (err.code) {
